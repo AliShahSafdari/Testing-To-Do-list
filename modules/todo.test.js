98a8c0d3101e8634da55c7483f603b4c92a579eb
paul-tes <paulasca8462@gmail.com>
2023-05-10 17:19:01 +0300

    added new test case in todo.test.js

--- modules/todo.test.js
+++ modules/todo.test.js
@@ -1,11 +1,22 @@
 const { addTask, deleteTask } = require('../_mocks_/todo.js');
 
 describe('We add the addtask and deleteTask functions ', () => {
-  test('Will add the task', () => {
+  test('when the user add a task it should be not empty on newItem', () => {
     const newItem = addTask(
       {
         task: 'Add the description for test',
         id: 1,
+        status: false,
+      },
+    );
+    expect(newItem).not.toBe([]);
+  });
+
+  test('when the user add a task it should be not empty on newItem', () => {
+    const newItem = addTask(
+      {
+        task: 'Test Task 2',
+        id: 2,
         status: false,
       },
     );
@@ -17,6 +28,6 @@ describe('We add the addtask and deleteTask functions ', () => {
       task: 'Add the description for test',
       id: 1,
       status: false,
-    })).toStrictEqual([]);
+    })).not.toStrictEqual([]);
   });
 });
