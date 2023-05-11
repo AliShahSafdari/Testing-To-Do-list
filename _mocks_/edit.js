eb81879d66d0e84e4dcbba707b38ac624291fcdc
Alishah Safdari <alishahsafdari@gmail.com>
2023-05-11 15:32:58 +0430

    Fix the linter error

--- _mocks_/edit.js
+++ _mocks_/edit.js
@@ -1,22 +1,22 @@
-const todos = [{ id: 1, task: "Hello alishah", status: false }, { id: 2, task: "Hello Sirat", status: false }];
+const todos = [{ id: 1, task: 'Hello alishah', status: false }, { id: 2, task: 'Hello Sirat', status: false }];
 const edit = (item) => {
-    const exactTodo = todos.find(todo => todo.id === item.id);
-    exactTodo.task = item.task;
-}
+  const exactTodo = todos.find((todo) => todo.id === item.id);
+  exactTodo.task = item.task;
+};
 
 const update = (id) => {
-    const exactTodo = todos.find(todo => todo.id === id);
-    exactTodo.status = true;
-    return exactTodo;
-}
+  const exactTodo = todos.find((todo) => todo.id === id);
+  exactTodo.status = true;
+  return exactTodo;
+};
 
 const clearAll = () => {
-    const exactTodo = todos.filter(todo => todo.status !== true);
-    exactTodo.forEach((todo, id) => {
-        todo.id = id + 1;
-    });
-    return exactTodo;
-}
+  const exactTodo = todos.filter((todo) => todo.status !== true);
+  exactTodo.forEach((todo, id) => {
+    todo.id = id + 1;
+  });
+  return exactTodo;
+};
 
 exports.edit = edit;
 exports.update = update;
--- modules/edit.test.js
+++ modules/edit.test.js
@@ -1,26 +1,26 @@
 const { edit, update, clearAll } = require('../_mocks_/edit.js');
 
-describe("implement edit the Task description and clear all ", () => {
-    test("edit the taks", () => {
-        const editItem = edit({
-            id: 1,
-            task: "Ok Alishah",
-            status: false
-        });
-        expect(editItem).toBe(editItem);
-    })
-    test("update the status", () => {
-        expect(update(1)).toStrictEqual({
-            id: 1,
-            task: "Ok Alishah",
-            status: true
-        })
+describe('implement edit the Task description and clear all ', () => {
+  test('edit the taks', () => {
+    const editItem = edit({
+      id: 1,
+      task: 'Ok Alishah',
+      status: false,
     });
-    test("clear all the complete task", () => {
-        expect(clearAll()).not.toBe([{
-            id: 1,
-            task: "Hello Sirat",
-            status: false,
-        }]);
-    })
+    expect(editItem).toBe(editItem);
+  });
+  test('update the status', () => {
+    expect(update(1)).toStrictEqual({
+      id: 1,
+      task: 'Ok Alishah',
+      status: true,
+    });
+  });
+  test('clear all the complete task', () => {
+    expect(clearAll()).not.toBe([{
+      id: 1,
+      task: 'Hello Sirat',
+      status: false,
+    }]);
+  });
 });
