342cd312bafd38a0c29bc5fb12c20f2f4f29dbd6
paul-tes <paulasca8462@gmail.com>
2023-05-11 15:52:52 +0300

    added edit and update test logics

--- modules/editUpdateClear.test.js
+++ modules/editUpdateClear.test.js
@@ -9,13 +9,24 @@ describe('implement edit the Task description and clear all ', () => {
     });
     expect(editItem).toBe(editItem);
   });
+
+  test('edit the taks', () => {
+    const editItem = editTask({
+      id: 1,
+      task: 'Ok Paul',
+      status: false,
+    });
+    expect(editItem).toBe(editItem);
+  });
+
   test('update the status', () => {
     expect(updateStatus(1)).toStrictEqual({
       id: 1,
-      task: 'Ok Alishah',
+      task: 'Ok Paul',
       status: true,
     });
   });
+
   test('clear all the complete task', () => {
     expect(clearAll()).not.toBe([{
       id: 1,
