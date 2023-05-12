a9fdefacf8f2807cd2f809db8500d8ace78e91e6
Alishah Safdari <alishahsafdari@gmail.com>
2023-05-12 08:10:15 +0430

    Update package.json file

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,7 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      // {
-      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      //   type: 'asset/resource',
-      // },
-    ],
+      }],
   },
   mode: 'development',
 };
